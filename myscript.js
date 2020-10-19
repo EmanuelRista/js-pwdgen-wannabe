eaad6a8edbaf0d0e985d65f3459a573f08801dd7
Emanuel <e.rista@outlook.it>
2020-10-19 14:13:02 +0200

    Finish

---
+++ myscript.js
@@ -0,0 +1,8 @@
+
+var nome = prompt('Ciao bello, qual è il tuo nome?')
+var cognome = prompt('E il cognome?')
+var colorepreferito = prompt('Qual è il tuo colore preferito?')
+
+var output = nome + cognome + colorepreferito + "19";
+
+document.getElementById("output").innerHTML = output;
